2b31aeee9e91ac566ab9d1feef166634ac4d8ff8
bennydrspc <bennydrm16@gmail.com>
2020-12-01 10:10:05 +0700

    id migration admins

--- database/migrations/2020_03_31_013832_create_informations_table.php
+++ database/migrations/2020_03_31_013832_create_informations_table.php
@@ -14,7 +14,7 @@ class CreateInformationsTable extends Migration
     public function up()
     {
         Schema::create('informations', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('judul');
             $table->text('konten');
             $table->integer('user_id')->unsigned();
--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -13,6 +13,6 @@ class AdminsTableSeeder extends Seeder
     public function run()
     {
         DB::insert("INSERT INTO admins (id, nip, user_id, nama, tempat_lahir, tanggal_lahir, janis_kelamin, agama, telp, alamat, foto, created_at, updated_at) VALUES
-        ('', '123456', 1, 'Zahir', 'Bogor', '2020-02-01', 'Laki-laki', 'Islam', '081-8181-8181', 'Bogor, Jawa Barat', NULL, '2020-02-18 20:10:26', '2020-03-29 20:27:27');");
+        (1, '123456', 1, 'Zahir', 'Bogor', '2020-02-01', 'Laki-laki', 'Islam', '081-8181-8181', 'Bogor, Jawa Barat', NULL, '2020-02-18 20:10:26', '2020-03-29 20:27:27');");
     }
 }
